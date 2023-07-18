fff329095e7f9b4757587e876ee7430a1501511b
TÜRKALP KUCUR <turkalp.kucur@gmail.com>
2023-07-18 20:48:09 +0300

    guncelleme
    guncelleme

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverApi = "http://test.wease.io:5002/api";
+const serverApi = "http://localhost:5002/api";
 
 export const getRoomExists = async (roomId) => {
   const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
